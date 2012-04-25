ddf84fa17f7a8af5378df141275d768e04645663
tzachs <tzach.solomon@gmail.com>
2012-04-25 17:08:19 +0300

    added insertQuestion

--- src/com/tzachsolomon/trivia/TriviaDbEngine.java
+++ src/com/tzachsolomon/trivia/TriviaDbEngine.java
@@ -1,6 +1,10 @@
 package com.tzachsolomon.trivia;
 
+import java.util.Calendar;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -101,13 +105,71 @@ public class TriviaDbEngine {
 	public void close() {
 		ourHelper.close();
 	}
+	
+	public long insertQuestion(long i_QuestionId, String i_Question, String i_Answer1, String i_Answer2,
+			String i_Answer3, String i_Answer4, int i_AnswerIndex, String i_Category, String i_SubCategory, 
+			String i_Language, long i_Correct, long i_Wrong, String i_DateCreated, String i_LastUpdate, boolean i_Enabled){
+		
+		long ret;
+		
+		this.open();
+		
+		ContentValues cv = new ContentValues();
+		
+		cv.put(KEY_ANSWER1, i_Answer1);
+		cv.put(KEY_ANSWER2, i_Answer2);
+		cv.put(KEY_ANSWER3, i_Answer3);
+		cv.put(KEY_ANSWER4, i_Answer4);
+		cv.put(KEY_ANSWER_INDEX, i_AnswerIndex);
+		cv.put(KEY_CATEGORY, i_Category);
+		cv.put(KEY_CORRECT, i_Correct);
+		cv.put(KEY_DATE_CREATED, i_DateCreated);
+		cv.put(KEY_ENABLED, i_Enabled);
+		cv.put(KEY_LANGUAGE, i_Language);
+		cv.put(KEY_LAST_UPDATE, i_LastUpdate);
+		cv.put(KEY_QUESTION, i_Question);
+		cv.put(KEY_QUESTIONID, i_QuestionId);
+		cv.put(KEY_SUB_CATEGORY, i_SubCategory);
+		cv.put(KEY_WRONG, i_Wrong);
+		
+		ret = ourDatabase.insert(TABLE_QUESTIONS, null, cv);
+		
+		this.close();
+		
+		return ret;
+	}
+	
+	public void createSampleQuestions(){
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(System.currentTimeMillis());
+		
+		insertQuestion(System.currentTimeMillis(), "Answer to the universe","42","Michael Jordan", "Nothing", "Checking long answer if fit",
+				1, "Litrature", "", "English", 0, 0, calendar.getTime().toString(), calendar.getTime().toString(), true);
+		insertQuestion(System.currentTimeMillis(), "Who wrote Lord of the rings","No1","Jrr", "Shlomo Oz", "Michale Crichton",
+				1, "Litrature", "", "English", 0, 0, calendar.getTime().toString(), calendar.getTime().toString(), true);
+		
+	}
 
 	public Question[] getQuestions() {
-		// 
-		Question[] ret = new Question[2];
+		//
+		String[] columns = { KEY_ANSWER1, KEY_ANSWER2, KEY_ANSWER3, KEY_ANSWER4, KEY_ANSWER_INDEX, KEY_CATEGORY,
+				KEY_CORRECT, KEY_DATE_CREATED, KEY_ENABLED, KEY_LANGUAGE, KEY_LAST_UPDATE, KEY_QUESTION, KEY_QUESTIONID,
+				KEY_ROWID, KEY_SUB_CATEGORY, KEY_WRONG };
+		
+		Cursor cursor;
+		int numberOfQuestions = -1;
+		this.open();
+		
+		cursor = ourDatabase.query(TABLE_QUESTIONS, columns, null, null, null, null, null);
+		numberOfQuestions = cursor.getCount();
+		
+		Question[] ret = new Question[numberOfQuestions];
 		
 		ret[0] = new Question();
 		ret[1] = new Question();
+		
+		this.close();
 		
 		return ret;
 	}
