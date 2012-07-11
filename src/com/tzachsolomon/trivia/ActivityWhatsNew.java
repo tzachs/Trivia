6ea3b20a8cbff78ce804135f760879ded20037d8
Tzach Solomon <tzach.solomon@gmail.com>
2012-07-11 22:13:27 +0300

    1. Whats new activity layout added

---
+++ src/com/tzachsolomon/trivia/ActivityWhatsNew.java
@@ -0,0 +1,15 @@
+package com.tzachsolomon.trivia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityWhatsNew extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		//
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_whats_new);
+	
+	}
+
+}
