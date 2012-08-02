f9b1456a4936febb3cb0ccbda1bb6cf0affe27af
Tzach Solomon <tzach.solomon@gmail.com>
2012-08-02 09:58:20 +0300

    1. Calclate timesPlayedRegionsAdded

--- src/com/tzachsolomon/trivia/Questions.java
+++ src/com/tzachsolomon/trivia/Questions.java
@@ -9,10 +9,12 @@ public class Questions {
 	public ArrayList<Question> m_Questions;
 	// holds the regions for how many times played
 	public ArrayList<Integer> m_RegionsHowManyTimesPlayed;
+	public int m_QuestionIndex;
 
 	public Questions() {
 		m_Questions = new ArrayList<Question>();
 		m_RegionsHowManyTimesPlayed = new ArrayList<Integer>();
+		m_QuestionIndex = 0;
 	}
 
 	public void addNewQuestion(Question question) {
@@ -21,19 +23,51 @@ public class Questions {
 
 	}
 
+	private void calculateRegionsTimesPlayed() {
+		int length = m_Questions.size() - 1;
+		int i = 1;
+		int timesPlayed;
+
+		if (length > 2) {
+
+			timesPlayed = m_Questions.get(0).getQuestionTimesPlayed();
+			
+			
+			while (i < length) {
+				if (timesPlayed != m_Questions.get(i).getQuestionTimesPlayed()) {
+					m_RegionsHowManyTimesPlayed.add(i);
+				}
+
+				i--;
+			}
+		}
+
+	}
+
 	public int getNumberOfQustions() {
-		// 
+		//
 		return m_Questions.size();
 	}
 
 	public void shuffle(boolean m_SortByNewQuestionFirst) {
-		// 
+		//
 		// TODO: if m_SortByNew is true, sort with regions
-		Collections.shuffle(m_Questions);
+		if (m_SortByNewQuestionFirst) {
+			calculateRegionsTimesPlayed();
+			myCollectionShuffle();
+			Collections.shuffle(m_Questions);
+		} else {
+			Collections.shuffle(m_Questions);
+		}
+	}
+
+	private void myCollectionShuffle() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public Question getQuestionAtIndex(int m_QuestionIndex) {
-		// 
+		//
 		return m_Questions.get(m_QuestionIndex);
 	}
 
