b3e458a3d9d6c0cb763721881acfcd3dea4bbce5
Tzach Solomon <tzach.solomon@gmail.com>
2012-09-17 11:01:44 +0300

    1. Added progress dialog when downloading scores

--- src/com/tzachsolomon/trivia/ActivityHighScores.java
+++ src/com/tzachsolomon/trivia/ActivityHighScores.java
@@ -20,11 +20,12 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ActivityHighScores extends Activity implements ScoreListener, OnClickListener {
+public class ActivityHighScores extends Activity implements ScoreListener,
+		OnClickListener {
 
 	private TableLayout table;
 	private TriviaDbEngine mTriviaDb;
-	
+
 	private SparseArray<String> mGameTypes;
 	private JSONHandler mJSONHandler;
 	private Button buttonGameTypeAll;
@@ -42,11 +43,23 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 		initializeVariables();
 		uploadLocalScores();
 		buttonGameTypeAll_Clicked();
-		
+
 	}
 
 	public class AsyncTaskDownloadScoresAndDisplay extends
 			AsyncTask<Integer, Integer, ContentValues[]> {
+
+		ProgressDialog mProgressDialog;
+
+		@Override
+		protected void onPreExecute() {
+			//
+			mProgressDialog = new ProgressDialog(ActivityHighScores.this);
+			mProgressDialog.setTitle("Downloding Game Scores");
+			mProgressDialog.setMessage("Downloading Game Scores from server");
+			mProgressDialog.setCancelable(false);
+			mProgressDialog.show();
+		}
 
 		@Override
 		protected void onPostExecute(ContentValues[] result) {
@@ -54,6 +67,8 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 
 			int row = 1;
 			TableRow tableRow;
+
+			mProgressDialog.dismiss();
 
 			if (result != null) {
 
@@ -63,9 +78,11 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 					row++;
 
 				}
-			}else{
-				Toast.makeText(ActivityHighScores.this, "Error downloading scores from server", Toast.LENGTH_LONG).show();
-			
+			} else {
+				Toast.makeText(ActivityHighScores.this,
+						"Error downloading scores from server",
+						Toast.LENGTH_LONG).show();
+
 			}
 
 		}
@@ -81,10 +98,9 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 
 	private void downloadScoresAndDisplay(int gameType) {
 		//
-		
+
 		AsyncTaskDownloadScoresAndDisplay a = new AsyncTaskDownloadScoresAndDisplay();
 
-		
 		clearTableView();
 		a.execute(gameType);
 
@@ -110,8 +126,6 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 		@Override
 		protected void onPreExecute() {
 
-				
-			
 			m_ProgressDialog.setCancelable(false);
 			m_ProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
 			m_ProgressDialog.setTitle("Uploading local scores");
@@ -138,8 +152,8 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 						row.getAsString(TriviaDbEngine.KEY_COL_GAME_SCORE),
 						row.getAsString(TriviaDbEngine.KEY_COL_GAME_TIME),
 						row.getAsInteger(TriviaDbEngine.KEY_ROWID)
-						
-						);
+
+				);
 				i++;
 				publishProgress(i);
 
@@ -174,8 +188,6 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 
 		mTriviaDb = new TriviaDbEngine(this);
 
-		
-
 		mGameTypes = new SparseArray<String>();
 
 		//
@@ -183,33 +195,31 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 				getString(R.string.soviet));
 		mGameTypes.put(ActivityGame.GAMETYPE_CATEGORIES,
 				getString(R.string.category));
-		mGameTypes.put(ActivityGame.GAMETYPE_LEVELS,
-				getString(R.string.levels));
+		mGameTypes
+				.put(ActivityGame.GAMETYPE_LEVELS, getString(R.string.levels));
 
 		table = (TableLayout) findViewById(R.id.tableLayoutHighScores);
-		
-		initializeButtons();
 
-		
+		initializeButtons();
 
 	}
 
 	private void initializeButtons() {
-		// 
-		buttonGameTypeAll = (Button)findViewById(R.id.buttonGameTypeAll);
-		buttonGameTypeCategory = (Button)findViewById(R.id.buttonGameTypeCategory);
-		buttonGameTypeLevel = (Button)findViewById(R.id.buttonGameTypeLevel);
-		buttonGameTypeSoviet = (Button)findViewById(R.id.buttonGameTypeSoviet);
-		
+		//
+		buttonGameTypeAll = (Button) findViewById(R.id.buttonGameTypeAll);
+		buttonGameTypeCategory = (Button) findViewById(R.id.buttonGameTypeCategory);
+		buttonGameTypeLevel = (Button) findViewById(R.id.buttonGameTypeLevel);
+		buttonGameTypeSoviet = (Button) findViewById(R.id.buttonGameTypeSoviet);
+
 		buttonGameTypeAll.setOnClickListener(this);
 		buttonGameTypeCategory.setOnClickListener(this);
 		buttonGameTypeLevel.setOnClickListener(this);
 		buttonGameTypeSoviet.setOnClickListener(this);
-		
+
 	}
-	
-	private void clearTableView () {
-		table.removeViews(1, table.getChildCount()-1);
+
+	private void clearTableView() {
+		table.removeViews(1, table.getChildCount() - 1);
 	}
 
 	private TableRow convertRowToTableRowView(int i_RowCounter,
@@ -229,7 +239,8 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 		type.setGravity(Gravity.CENTER);
 		date.setGravity(Gravity.CENTER);
 
-		type.setText(getGameTypeStringByGameTypeId(row.getAsInteger(TriviaDbEngine.KEY_COL_GAME_TYPE)));
+		type.setText(getGameTypeStringByGameTypeId(row
+				.getAsInteger(TriviaDbEngine.KEY_COL_GAME_TYPE)));
 		user.setText(row.getAsString(TriviaDbEngine.KEY_COL_USERNAME));
 		score.setText(row.getAsString(TriviaDbEngine.KEY_COL_GAME_SCORE));
 		dateFormat = new Date(row.getAsLong(TriviaDbEngine.KEY_COL_GAME_TIME));
@@ -248,7 +259,7 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 
 	private CharSequence getGameTypeStringByGameTypeId(int gameType) {
 		//
-		String ret =mGameTypes.get(gameType); 
+		String ret = mGameTypes.get(gameType);
 
 		if (ret != null) {
 			return ret;
@@ -257,7 +268,7 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 		}
 
 	}
-	
+
 	@Override
 	public void onScoreAdded(int i_Result) {
 		//
@@ -274,7 +285,7 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 
 	@Override
 	public void onClick(View v) {
-		// 
+		//
 		switch (v.getId()) {
 		case R.id.buttonGameTypeAll:
 			buttonGameTypeAll_Clicked();
@@ -292,31 +303,31 @@ public class ActivityHighScores extends Activity implements ScoreListener, OnCli
 		default:
 			break;
 		}
-		
+
 	}
 
 	private void buttonGameTypeAll_Clicked() {
-		// 
+		//
 		downloadScoresAndDisplay(0);
-		
+
 	}
 
 	private void buttonGameTypeCategory_Clicked() {
-		// 
+		//
 		downloadScoresAndDisplay(ActivityGame.GAMETYPE_CATEGORIES);
-		
+
 	}
 
 	private void buttonGameTypeLevel_Clicked() {
-		// 
+		//
 		downloadScoresAndDisplay(ActivityGame.GAMETYPE_LEVELS);
-		
+
 	}
 
 	private void buttonGameTypeSoviet_Clicked() {
-		// 
+		//
 		downloadScoresAndDisplay(ActivityGame.GAMETYPE_ALL_QUESTIONS);
-		
+
 	}
 
 }
